f6752a429eecea8adbaf6ce849dec3751cf51501
lana <artist158@naver.com>
2022-11-24 20:34:57 +0900

    [FEAT] gradle:kapt, activity_main, activity_add_memo

---
+++ app/src/main/java/com/example/kotlin_android_week8/AddMemoActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin_android_week8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin_android_week8.databinding.ActivityAddMemoBinding
+
+class AddMemoActivity : AppCompatActivity() {
+    private val viewBinding: ActivityAddMemoBinding by lazy {
+        ActivityAddMemoBinding.inflate(layoutInflater)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+
+
+    }
+}
