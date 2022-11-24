4c97b44b3d1da796c0a0cf0f9d75f387e9ff7a22
lana <artist158@naver.com>
2022-11-25 00:36:26 +0900

    [FIX] connect adapter

--- app/src/main/java/com/example/kotlin_android_week8/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_android_week8/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.kotlin_android_week8
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Selection.selectAll
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlin_android_week8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +22,19 @@ class MainActivity : AppCompatActivity() {
         val roomDb = AppDatabase!!.getInstance(this)
 
         if (roomDb != null) {
+            roomDb.memoDao().insert(Memo("db save check", "please!!!"))
             val savedMemo = roomDb.memoDao().selectAll()
             memoList.addAll(savedMemo)
         }
 
         val memoDataRVAdapter = MemoDataRVAdapter(memoList)
+
+        viewBinding.rvMemoBox.adapter = memoDataRVAdapter
+        viewBinding.rvMemoBox.layoutManager = LinearLayoutManager(this)
+
+        viewBinding.btnNewMemo.setOnClickListener(){
+            var intent = Intent(this, AddMemoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
